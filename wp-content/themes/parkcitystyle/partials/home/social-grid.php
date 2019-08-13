988fd96cc7aed6baf14f70d56ed2a105292bdb10
Stephen Williams <stephen@wearetmbr.com>
2019-08-13 14:07:39 -0600

    social feed

--- wp-content/themes/parkcitystyle/partials/home/social-grid.php
+++ wp-content/themes/parkcitystyle/partials/home/social-grid.php
@@ -1,7 +1,7 @@
 <div class="social-grid wow fadeInUp">
 	<div class="social-overlay"></div>
   <h2 class="center">Get Social</h2>
-  <?php juicer_feed('name=jhstylemagazine&per=6&pages=1'); ?>
+  <?php juicer_feed("name=pcstylemag"); ?>
 </div>
 <div class="center">
 	<a href="/social/" class="btn center">View More</a>
